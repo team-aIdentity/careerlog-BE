8fbbb8e0984e3580b564c49150ab6b2052019328
이형진 <ihyeongjin@ihyeongjin-ui-MacBookPro.local>
2024-12-12 12:06:17 +0900

    feat: image uploads

---
+++ src/uploads/uploads.controller.ts
@@ -0,0 +1,21 @@
+import {
+  Controller,
+  HttpCode,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadsService } from './uploads.service';
+
+@Controller('uploads')
+export class UploadsController {
+  constructor(private readonly uploadsService: UploadsService) {}
+
+  @Post('image')
+  @UseInterceptors(FileInterceptor('file'))
+  @HttpCode(200)
+  async saveImage(@UploadedFile() file: Express.Multer.File) {
+    return await this.uploadsService.imageUpload(file);
+  }
+}
