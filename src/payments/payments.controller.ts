b270fd46cbddb2e7d7d01580afebb9969059291c
이형진 <ihyeongjin@ihyeongjin-ui-MacBookPro.local>
2024-12-13 20:34:11 +0900

    payment

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { join } from 'path';
+import { TossPaymentDto } from './dto/tossPayments.dto';
+import { PaymentsService } from './payments.service';
 
 @Controller('payments')
 export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
   @Get('/success')
-  success(@Res() res: Response): void {}
+  async success(@Res() res: Response) {
+    console.log(join(__dirname, '..', '..', 'public', 'success.html'));
+    res.sendFile(join(__dirname, '..', '..', 'public', 'success.html'));
+  }
+
+  @Get('fail')
+  fail(@Res() res: Response) {
+    console.log('asdfasdfasdfasdf');
+    console.log(join(__dirname, '..', '..', 'public', 'fail.html'));
+    res.sendFile(join(__dirname, '..', '..', 'public', 'fail.html'));
+  }
+
+  @Post('toss')
+  tossPayments(@Body() tossPaymentDto: TossPaymentDto) {
+    return this.paymentsService.tossPayment(tossPaymentDto);
+  }
 }
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { TossPaymentDto } from './dto/tossPayments.dto';
+import axios from 'axios';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+  private readonly tossURL = 'https://api.tosspayments.com/v1/payments';
+  private readonly secretKey = process.env.TOSS_SECRET_KEY;
+
+  async tossPayment(tossPaymentDto: TossPaymentDto) {
+    console.log('>>>>>>>>>', this.secretKey);
+    const { orderId, amount, paymentKey } = tossPaymentDto;
+
+    try {
+      const response = await axios.post(
+        `${this.tossURL}/${paymentKey}`,
+        {
+          orderId,
+          amount,
+        },
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(`${this.secretKey}:`).toString()}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      return {
+        title: '결제 성공',
+        body: response.data,
+      };
+    } catch (error) {
+      throw new BadRequestException({ message: '결제 실패', data: error });
+    }
+  }
+}
