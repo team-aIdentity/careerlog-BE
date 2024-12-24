bffc3fe149ba16b98fd22e0b2af4321b163c1853
이형진 <ihyeongjin@ihyeongjin-ui-MacBookPro.local>
2024-12-24 18:28:25 +0900

    fix: language module

--- src/language/language.module.ts
+++ src/language/language.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { LanguageService } from './language.service';
 import { LanguageController } from './language.controller';
+import { JwtService } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtAccessAuthGuard } from 'src/auth/jwt/jwtAccessAuth.guard';
+import { UserModule } from 'src/user/user.module';
+import { UserService } from 'src/user/user.service';
+import { Language } from './entity/language.entity';
 
 @Module({
-  providers: [LanguageService],
-  controllers: [LanguageController]
+  imports: [TypeOrmModule.forFeature([Language]), UserModule],
+  providers: [LanguageService, JwtAccessAuthGuard, JwtService, UserService],
+  controllers: [LanguageController],
 })
 export class LanguageModule {}
