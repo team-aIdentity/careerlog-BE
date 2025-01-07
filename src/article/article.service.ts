860aef504660556f958e2cebf6ffb0aa20c8b27d
이형진 <ihyeongjin@ihyeongjin-ui-MacBookPro.local>
2025-01-08 00:39:34 +0900

    update: review get

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -64,13 +64,13 @@ export class ArticleService {
       case 'like':
         query
           .leftJoin('article.userSaved', 'userSaved')
-          .addSelect('COUNT(userSaved.id)', 'userSavedCount')
+          .addSelect('COUNT(article.userSaved.id)', 'userSavedCount')
           .groupBy('article.id')
           .addGroupBy('user.id')
           .addGroupBy('profile.id')
           .addGroupBy('category.id')
           .addGroupBy('job.id')
-          .orderBy('COUNT(userSaved.id)', 'DESC');
+          .orderBy('COUNT(article.userSaved.id)', 'DESC');
         break;
       default:
         query.orderBy('article.createdAt', 'DESC');
